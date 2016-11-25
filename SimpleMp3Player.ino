773e227c27fe53b16be67ed75a0f8c947d9d8854
weidingerhp <hans-peter@hp-weidinger.at>
2016-11-25 20:36:12 +0100

    Projektdateien hinzufügen.

---
+++ SimpleMp3Player.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SimpleMp3Player.ino
+ Created:	25.11.2016 20:35:57
+ Author:	hpw
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
